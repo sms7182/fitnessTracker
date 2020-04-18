074f71c8e8997c4acffbd8c2fd90a3043aa4fce2
sms7182 <sms7182@gmail.com>
2020-04-18 14:52:37 +0430

    add selector controller

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,9 +9,12 @@ import {MatCheckboxModule} from '@angular/material/checkbox'
 import {MatSidenavModule} from '@angular/material/sidenav'
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatListModule} from '@angular/material/list'
+import {MatTabsModule} from '@angular/material/tabs'
+import {MatCardModule} from '@angular/material/card'
+import {MatSelectModule} from '@angular/material/select'
 @NgModule({
-    imports:[MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatCheckboxModule,MatSidenavModule,MatToolbarModule,MatListModule],
-    exports:[MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatCheckboxModule,MatSidenavModule,MatToolbarModule,MatListModule]
+    imports:[MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatCheckboxModule,MatSidenavModule,MatToolbarModule,MatListModule,MatTabsModule,MatCardModule,MatSelectModule],
+    exports:[MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatCheckboxModule,MatSidenavModule,MatToolbarModule,MatListModule,MatTabsModule,MatCardModule,MatSelectModule]
 })
 export class MaterialModule{
 
